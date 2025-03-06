38cad4468b010ce4f97834f860067223270518af
RivkaSchonfeld <rivkashon5@gmail.com>
2025-03-07 00:28:47 +0200

    added react with two functions using axios

--- src/Api.js
+++ src/Api.js
@@ -5,15 +5,25 @@ const url = "https://localhost:7071/api/Client"
 export const getAllClients = async () => {
     try {
         const list = await axios.get(url)
-        console.log("list", list.data)
         return list.data
 
     }
     catch (ex) {
         console.log(ex)
-        return "hello"
+        return []
     }
 
 
 
+}
+
+export const getClientById= async (id)=>{
+    try{
+        const client = await axios.get(url+"/"+id)
+        return client.data
+    }
+    catch(ex){
+        console.log(ex)
+
+    }
 }
--- src/App.js
+++ src/App.js
@@ -1,49 +1,67 @@
-import { getAllClients } from './Api';
+import { getAllClients, getClientById } from './Api';
 import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
   const [clients, setclients] = useState([])
+  const [idClient, setIdClient] = useState("")
+  const [CLient, setClient] = useState()
 
   useEffect((() => {
-    getAllClients()
+    getAllClients()//getting all the clients, filling the list
       .then(data => {
         setclients(data)
-        console.log(data)
       })
+
   }), [])
 
-  return (
+  const getTheClientById = () => {
+    getClientById(idClient)
+      .then(data => setClient(data))
+      .catch(() => {
+        alert("please enter a valid id")
+        console.log("none")
+      })
+}
+
+
+return (
+  <div>
+    <h1>Clients</h1>
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Address</th>
+          <th>Date Of Birth</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+
+      {!clients || clients.length === 0 ? <tbody><tr><td>loading data...</td></tr></tbody> :
+
+        <tbody>
+          {
+            clients.map((p, i) => (
+              <tr key={i}>
+                <td>{p.id}</td>
+                <td>{p.name}</td>
+                <td>{p.address}</td>
+                <td>{p.dateOfBirth}</td>
+                <td>{p.email}</td>
+              </tr>))
+          }
+        </tbody>}
+    </table>
+
     <div>
-      <h1>Clients</h1>
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Date Of Birth</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-
-        {!clients || clients.length === 0 ? <tbody><tr><td>loading data...</td></tr></tbody> :
-
-          <tbody>
-            {
-              clients.map((p, i) => (
-                <tr key={i}>
-                  <td>{p.id}</td>
-                  <td>{p.name}</td>
-                  <td>{p.address}</td>
-                  <td>{p.dateOfBirth}</td>
-                  <td>{p.email}</td>
-                </tr>))
-            }
-          </tbody>}
-      </table>
+      <input placeholder="enter client id" value={idClient} onChange={(i) => setIdClient(i.target.value)} />
+      <button onClick={() => getTheClientById()}>Submit</button>
+      {CLient && <p>{CLient.name}</p>}
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
